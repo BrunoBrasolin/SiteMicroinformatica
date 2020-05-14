39c18fb7e519874572c2182001862f347b7ef5ef
Bruno Brasolin <brunobrasolinc@gmail.com>
2020-05-14 08:17:01 -0300

    Updates

---
+++ precache-manifest.ac7ab7476731c86e09aa13ce15a597cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6b49c9be82ca882ebe911fda8ce189",
+    "url": "/SiteMicroinformatica/index.html"
+  },
+  {
+    "revision": "0f867d0c6bfeb53703eb",
+    "url": "/SiteMicroinformatica/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6aa9a9e30241b30ac9c5",
+    "url": "/SiteMicroinformatica/static/css/main.32fba570.chunk.css"
+  },
+  {
+    "revision": "0f867d0c6bfeb53703eb",
+    "url": "/SiteMicroinformatica/static/js/2.ad61fa00.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/SiteMicroinformatica/static/js/2.ad61fa00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aa9a9e30241b30ac9c5",
+    "url": "/SiteMicroinformatica/static/js/main.92b66b71.chunk.js"
+  },
+  {
+    "revision": "723687ba5562d3d39ef5",
+    "url": "/SiteMicroinformatica/static/js/runtime-main.9b732b22.js"
+  },
+  {
+    "revision": "a8402539be5017c835533677a5024db0",
+    "url": "/SiteMicroinformatica/static/media/lousa.a8402539.jpg"
+  }
+]);
